96b8df1f8e0d477d16818a39ff07dceb4d700b38
Sergey Sedov <sidslog@Sergeys-Mac-Pro.local>
2015-09-04 17:44:17 +0300

    Test signal and producer

---
+++ RCTest/TestSignal.swift
@@ -0,0 +1,13 @@
+//
+//  TestSignal.swift
+//  RCTest
+//
+//  Created by Sergey Sedov on 04/09/15.
+//  Copyright (c) 2015 Sergey Sedov. All rights reserved.
+//
+
+import UIKit
+
+class TestSignal: NSObject {
+   
+}
