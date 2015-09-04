8fa622158e74df4a85851072df48452569758a4e
Sergey Sedov <sidslog@Sergeys-Mac-Pro.local>
2015-09-04 16:17:42 +0300

    Initial Commit

---
+++ RCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RCTest
+//
+//  Created by Sergey Sedov on 04/09/15.
+//  Copyright (c) 2015 Sergey Sedov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
